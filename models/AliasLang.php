243c2e0436937266b1a35f4298ca359944647a51
Fabio Maggio <fabio@infoweb.be>
2015-06-04 14:25:57 +0200

    Update unique validator
    The validator is changed so that there is also a check against the alias'
    entityType (in combination with the language and aliasId).

--- models/AliasLang.php
+++ models/AliasLang.php
@@ -14,6 +14,11 @@ use Yii;
 class AliasLang extends \yii\db\ActiveRecord
 {
     /**
+     * The entity type of the coupled Alias
+     */
+    public $entityType = null;
+    
+    /**
      * @inheritdoc
      */
     public static function tableName()
@@ -39,12 +44,22 @@ class AliasLang extends \yii\db\ActiveRecord
             [['url'], 'string', 'max' => 255],
             [['alias_id', 'language'], 'unique', 'targetAttribute' => ['alias_id', 'language'], 'message' => Yii::t('infoweb/alias', 'The combination of Alias ID and Language has already been taken.')],
             [['language', 'url'], 'unique', 'targetAttribute' => ['language', 'url'], 'message' => Yii::t('infoweb/alias', 'The combination of Language and Url has already been taken.'), 'filter' => function($query) {
-                return (!$this->isNewRecord) ? $query->andWhere(['!=', 'alias_id', $this->alias_id]) : $query;
+                // If an entityType is provided, use it to match with the alias table
+                // because an url has to be unique within a type of entity    
+                if ($this->entityType) {
+                    $query->innerJoin('alias', ['alias.entity' => $this->entityType, 'alias.id' => 'alias_lang.alias_id']);
+                }
+                
+                if (!$this->isNewRecord) {
+                    $query->andWhere(['!=', 'alias_id', $this->alias_id]);    
+                }
+
+                return $query;
             }],
             ['url', function($attribute, $params) {
-
+                // Check if the url is not a reserved url
                 if (in_array($this->url, Yii::$app->getModule('alias')->reservedUrls))
-                    $this->addError($attribute, Yii::t('infoweb/alias', 'This is a reserved url and can not be used'));   
+                    $this->addError($attribute, Yii::t('infoweb/alias', 'This is a reserved url and can not be used'));
             }]
         ];
     }
