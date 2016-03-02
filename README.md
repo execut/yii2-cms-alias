1147001f94bf836619a89704aabd108619b8772d
Fabio Maggio <fabiomaggio@gmail.com>
2016-03-02 16:00:38 +0100

    Replace behavior for AliasRelation with a trait

--- README.md
+++ README.md
@@ -49,8 +49,8 @@ To use the module, execute yii migration
 yii migrate/up --migrationPath=@vendor/infoweb-internet-solutions/yii2-cms-alias/migrations
 ```
 
-Behaviors
----------
+Behavior
+--------
 
 The `AliasBehavior` is used to manage the `url` field for the entity that the `Alias` is attached to.
 The behavior has to be attached to an `ActiveRecord` class that has a language identifier.
@@ -85,19 +85,17 @@ The `url` field can be rendered in your `ActiveForm` view
     ]) ?>
 ```
 
-The `AliasRelationBehavior` is used to extend the entity for which you want to enable the usage of `Alias`.
-It defines custom event(s) and `getAlias` relation for the model.
+Trait
+-----
+The `AliasRelationTrait` is used to extend the entity for which you want to enable the usage of `Alias`.
+It defines custom event(s), the`getAlias` relation for the model and the `findByAlias` method.
 Below is an example of how it can be attached to `infoweb\pages\models\Page`
 
 ```php
-use infoweb\alias\behaviors\AliasRelationBehavior;
+use infoweb\alias\traits\AliasRelationTrait;
 
-public function behaviors()
+class Page extends ActiveRecord
 {
-    return [
-        'aliasRelation' => [
-            'class' => AliasRelationBehavior::className(),
-        ],
-    ];
+	use AliasRelationTrait;
 }
 ```
---
+++ traits/AliasRelationTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace infoweb\alias\traits;
+
+use yii;
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use infoweb\alias\models\Alias;
+
+trait AliasRelationTrait
+{
+    /**
+     * Finds an instance of the owner based on the provided alias and language
+     *
+     * @param string $alias The alias
+     * @param mixed $language The language
+     * @return ActiveRecord An instance of the owner
+     */
+    public static function findByAlias($alias = '', $language = null)
+    {
+        // Try to load an Alias with provided params
+        $aliasModel = Alias::findOne([
+            'url' => $alias,
+            'language' => ($language) ?: Yii::$app->language,
+            'entity' => parent::className(),
+        ]);
+
+        return parent::findOne($aliasModel->entity_id);
+    }
+
+    public function events()
+    {
+        $events = (method_exists(parent, 'events')) ? parent::events() : [];
+        return ArrayHelper::merge($events, [
+            ActiveRecord::EVENT_AFTER_DELETE => 'afterDelete'
+        ]);
+    }
+
+    public function afterDelete()
+    {
+        parent::afterDelete();
+
+        // Delete all attached aliases
+        $this->deleteAliases();
+    }
+
+    /**
+     * Returns the attached Alias model
+     *
+     * @return Alias
+     */
+    public function getAlias($language = null)
+    {
+        return $this->getTranslation($language)->alias;
+    }
+
+    /**
+     * Deletes all aliases for the model
+     *
+     * @return boolean
+     */
+    protected function deleteAliases()
+    {
+        return Alias::deleteAll(['entity' => parent::className(), 'entity_id' => $this->primaryKey]);
+    }
+}
