4efeb41fda517d37a94f0007dafe37ff26e76761
user <user@email.com>
2016-11-06 20:41:43 +0300

    Postgresql adaptation

--- migrations/m141010_135644_init.php
+++ migrations/m141010_135644_init.php
@@ -5,44 +5,48 @@ use yii\db\Migration;
 
 class m141010_135644_init extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $tableOptions = null;
         
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
+
+        if ($this->db->driverName === 'pgsql') {
+            $this->execute('CREATE TYPE alias_type AS ENUM (\'system\',\'user-defined\')');
+        }
         
         // Create '' table
         $this->createTable('{{%alias}}', [
-            'id'            => Schema::TYPE_PK,
-            'type'          => "ENUM('system','user-defined') NOT NULL DEFAULT 'user-defined'",
-            'entity'        => Schema::TYPE_STRING . '(50) NOT NULL',
-            'entity_id'     => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'created_at'    => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'    => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'id'            => $this->primaryKey(),
+            'type'          => "alias_type NOT NULL DEFAULT 'user-defined'",
+            'entity'        => $this->string(50)->notNull(),
+            'entity_id'     => $this->integer()->unsigned()->notNull(),
+            'created_at'    => $this->integer()->unsigned()->notNull(),
+            'updated_at'    => $this->integer()->unsigned()->notNull(),
         ], $tableOptions);
         
-        $this->createIndex('entity_entity_id', '{{%alias}}', ['entity', 'entity_id'], true);
-        $this->createIndex('entity_id', '{{%alias}}', 'entity_id');
+        $this->createIndex('alias_entity_entity_id_i', '{{%alias}}', ['entity', 'entity_id'], true);
+        $this->createIndex('alias_entity_id_i', '{{%alias}}', 'entity_id');
 
         // Create 'alias_lang' table
         $this->createTable('{{%alias_lang}}', [
-            'alias_id'      => Schema::TYPE_INTEGER . ' NOT NULL',
-            'language'      => Schema::TYPE_STRING . '(10) NOT NULL',
-            'url'           => Schema::TYPE_STRING . '(255) NOT NULL',
-            'created_at'    => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'    => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'alias_id'      => $this->integer()->notNull(),
+            'language'      => $this->string(10)->notNull(),
+            'url'           => $this->string()->notNull(),
+            'created_at'    => $this->integer()->unsigned()->notNull(),
+            'updated_at'    => $this->integer()->unsigned()->notNull(),
         ], $tableOptions);
 
         $this->addPrimaryKey('alias_id_language', '{{%alias_lang}}', ['alias_id', 'language']);
         //$this->createIndex('language_url', '{{%alias_lang}}', ['language', 'url'], true);
-        $this->createIndex('language', '{{%alias_lang}}', 'language');
-        $this->createIndex('url', '{{%alias_lang}}', 'url');
+        $this->createIndex('alias_lang_language_i', '{{%alias_lang}}', 'language');
+        $this->createIndex('alias_lang_url_i', '{{%alias_lang}}', 'url');
         $this->addForeignKey('FK_ALIAS_LANG_ALIAS_ID', '{{%alias_lang}}', 'alias_id', '{{%alias}}', 'id', 'CASCADE', 'RESTRICT');
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->dropTable('alias_lang');
         $this->dropTable('alias');
--- migrations/m160212_075225_create_new_alias_table.php
+++ migrations/m160212_075225_create_new_alias_table.php
@@ -5,25 +5,25 @@ use yii\db\Migration;
 
 class m160212_075225_create_new_alias_table extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $this->dropTable('{{%alias_lang}}');
         $this->dropTable('{{%alias}}');
 
         $this->createTable('{{%alias}}', [
-            'entity'        => Schema::TYPE_STRING . '(255) NOT NULL',
-            'entity_id'     => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'language'      => Schema::TYPE_STRING . '(10) NOT NULL',
-            'url'           => Schema::TYPE_STRING . '(255) NOT NULL',
-            'type'          => "ENUM('system','user-defined') NOT NULL DEFAULT 'user-defined'",
-            'created_at'    => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'    => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'entity'        => $this->string()->notNull(),
+            'entity_id'     => $this->integer()->unsigned()->notNull(),
+            'language'      => $this->string(10)->notNull(),
+            'url'           => $this->string()->notNull(),
+            'type'          => "pages_type NOT NULL DEFAULT 'user-defined'",
+            'created_at'    => $this->integer()->unsigned()->notNull(),
+            'updated_at'    => $this->integer()->unsigned()->notNull(),
         ]);
 
         $this->addPrimaryKey('entity_entity_id_language', '{{%alias}}', ['entity', 'entity_id', 'language']);
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->dropTable('alias');
     }
